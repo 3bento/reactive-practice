f39a0a5e75a452dec12594e528964e46d6be1188
kenny <duh747@gmail.com>
2022-07-16 23:18:09 -0300

    mutiny-parent > hello > Added UniAdvanced, I don't understand it yet!

---
+++ mutiny-parent/hello/src/main/java/dev/ebento/hellomutiny/pipelines/uni/UniAdvanced.java
@@ -0,0 +1,30 @@
+package dev.ebento.hellomutiny.pipelines.uni;
+
+import io.smallrye.mutiny.Uni;
+import org.jboss.logging.Logger;
+
+public class UniAdvanced {
+
+    private static final Logger LOG = Logger.getLogger(UniAdvanced.class);
+
+    public static void main(String[] args) {
+        Uni<String> uni = Uni.createFrom().emitter(em -> {
+
+            String result = "A";
+
+            em.complete(result);
+        });
+        Uni<String> uni2 = Uni.createFrom().completionStage(uni
+                        .onItem().transform(e -> e + " B")
+                .subscribe().asCompletionStage());
+
+        Uni<String> uni3 = Uni.createFrom()
+                .completionStage(uni2.onItem().transform(e -> e + " C")
+                        .subscribe().asCompletionStage());
+
+        Uni<String> uni4 = Uni.createFrom()
+                .completionStage(uni3.onItem().transform(e -> e + " D").subscribe().asCompletionStage());
+
+        uni4.subscribe().with(LOG::info);
+    }
+}
